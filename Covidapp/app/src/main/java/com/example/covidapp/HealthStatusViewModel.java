021b9b3bcef540b1fa00f0251bedd66de122c649
CulturedMan <77681600+CulturedMan@users.noreply.github.com>
2022-09-29 01:39:36 +0200

    Admin Side

--- Covidapp/app/src/main/java/com/example/covidapp/HealthStatusViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.covidapp;
-
-import androidx.lifecycle.ViewModel;
-
-public class HealthStatusViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Covidapp/app/src/main/java/com/example/covidapp/PCRTestViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.covidapp;
-
-import androidx.lifecycle.ViewModel;
-
-public class PCRTestViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
